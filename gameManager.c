80fc2598296e744fa2c4568523e8ce47e9aa2324
10v-maks-rybak <maks.rybak@tenvalleys.com>
2025-11-06 20:02:47 +0100

    test commit removed

--- gameManager.c
+++ gameManager.c
@@ -16,7 +16,7 @@ void GameManager_Init(GameManager *gm) {
     //TO-DO Init players and needed data
     printf("Game initialized!\n");
 
-    GameBoard_Print(&gm->gb); //test
+    GameBoard_Print(&gm->gb);
 }
 
 void GameManager_Run(GameManager *gm) {
