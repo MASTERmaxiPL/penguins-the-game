8673643edbe1e86d7a4502aa5d5137d4119fb0f8
13524Olek <105112075+13524Olek@users.noreply.github.com>
2025-11-25 20:25:44 +0100

    Update movementPhase.h

--- movementPhase.h
+++ movementPhase.h
@@ -1,6 +1,8 @@
 #ifndef MOVEMENT_PHASE_H
 #define MOVEMENT_PHASE_H
 
+#include "gameManager.h"
+
 static const int DIRS[4][2];
 bool GameBoard_IsInBounds(const GameBoard *gb, int x, int y);
 bool GameBoard_IsValidMove(const GameBoard *gb, int sx, int sy, int ex, int ey);
