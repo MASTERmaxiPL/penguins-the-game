0a0654cc87f3819269b3b92251e32de2b9e110e0
marcel603 <marcelpeda7@gmail.com>
2025-11-26 19:32:22 +0100

    Update movementPhase.c

--- movementPhase.c
+++ movementPhase.c
@@ -41,3 +41,43 @@ bool MovementPhase_IsValidMove(const GameBoard *gb, int startx, int starty, int 
 
     return true;
 }
+
+
+bool MovementPhase_MovePenguin(GameBoard gb, int startx, int starty, int endx, int endy) {
+    if (!Movement_IsValidMove(gb, startx, starty, endx, endy))
+        return false;
+
+    IceFloestart = &gb->floeGrid[starty][stratx];
+    IceFloe target = &gb->floeGrid[endy][endx];
+
+    target->occupantId = start->occupantId;
+
+    start->isFloating = false;
+    start->fishCount = 0;
+    start->occupantId = -1;
+
+    return true;
+}
+
+void MovementPhase_Movement(GameBoardgb, int currentPlayer) {
+    int startx, starty, endx, endy;
+
+    printf("Player %d, choose penguin to move (x y): ", currentPlayer + 1);
+    scanf("%d %d", &startx, &starty);
+
+    if (!MovmentPhase_IsInBounds(gb, startx, starty) ||
+        gb->floeGrid[starty][startx].occupantId != currentPlayer) 
+    {
+        printf("Invalid penguin.\n");
+        return;
+    }
+
+    printf("Choose destination (x y): ");
+    scanf("%d %d", &endx, &endy);
+
+    if (MovementPhase_MovePenguin(gb, startx, starty, endx, endy)) {
+        printf("Move successful!\n");
+    } else {
+        printf("Invalid move.\n");
+    }
+}
