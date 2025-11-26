b12a574aa589b6dd3f2856e90666a783dd44f012
13524Olek <105112075+13524Olek@users.noreply.github.com>
2025-11-26 22:05:49 +0100

    Update movementPhase.h

--- movementPhase.h
+++ movementPhase.h
@@ -3,6 +3,18 @@
 
 #include "gameManager.h"
 
+typedef struct {
+    bool isFloating;
+    int fishCount;
+    int occupantId;
+} IceFloe;
+
+typedef struct {
+    int boardWidth;
+    int boardHeight;
+    IceFloe **floeGrid;
+} GameBoard;
+
 bool MovementPhase_IsInBounds(const GameBoard *gb, int x, int y);
 bool MovementPhase_IsValidMove(const GameBoard *gb, int startx, int starty, int endx, int endy);
 bool MovementPhase_MovePenguin(GameBoard gb, int startx, int starty, int endx, int endy);
