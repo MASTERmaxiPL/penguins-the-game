57db0adb8070930614f1b6206a1358f85915db52
MASTERmaxiPL <maks.rybak@op.pl>
2025-11-26 22:21:50 +0100

    added important import - gameBoard and removed random garbage

--- movementPhase.h
+++ movementPhase.h
@@ -1,23 +1,12 @@
 #ifndef MOVEMENT_PHASE_H
 #define MOVEMENT_PHASE_H
 
+#include "gameBoard.h"
 #include "gameManager.h"
-
-typedef struct {
-    bool isFloating;
-    int fishCount;
-    int occupantId;
-} IceFloe;
-
-typedef struct {
-    int boardWidth;
-    int boardHeight;
-    IceFloe **floeGrid;
-} GameBoard;
 
 bool MovementPhase_IsInBounds(const GameBoard *gb, int x, int y);
 bool MovementPhase_IsValidMove(const GameBoard *gb, int startx, int starty, int endx, int endy);
 bool MovementPhase_MovePenguin(GameBoard gb, int startx, int starty, int endx, int endy);
-void MovementPhase_Movement(GameBoardgb, int currentPlayer);
+void MovementPhase_Movement(GameBoard gb, int currentPlayer);
 
 #endif
