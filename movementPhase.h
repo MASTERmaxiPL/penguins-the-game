5c9b6ac91b0face42114100f1214a1c9d9bb3f82
13524Olek <105112075+13524Olek@users.noreply.github.com>
2025-12-07 23:21:45 +0100

    Update movementPhase.h

--- movementPhase.h
+++ movementPhase.h
@@ -10,7 +10,8 @@ bool Check_Player_Has_Any_Moves(const GameBoard *gb, int currentPlayerIndex);
 bool Check_Penguin_Has_Any_Moves(const GameBoard *gb, int posX, int posY, int boardHeight, int boardWidth, int currentPlayerIndex);
 bool Is_Move_In_Bounds(const GameBoard *gb, int x, int y);
 bool Is_Valid_Move(const GameBoard *gb, int startX, int startY, int endX, int endY);
-bool Move_Penguin(const GameBoard *gb, int startX, int startY, int endX, int endY);
-void Player_Movement_Turn(const GameBoard *gb, int currentPlayerIndex);
+bool Move_Penguin(GameManager *gm, int startX, int startY, int endX, int endY);
+void Player_Movement_Turn(GameManager *gm, int currentPlayerIndex);
+void Print_Final_Scores(const GameManager *gm);
 
 #endif
